ddffccf45329cf14c27d015c04b076c5585219d3
chopinic <yangguangbus@163.com>
2023-04-18 22:09:00 +0800

    delete sycl

--- cudatest.cu
+++ cudatest.cu
@@ -9,19 +9,10 @@ __global__ void factorial(const int N, int *f){
   }
 }
 
+using namespace std;
 
 int main()
 {
-    auto devices = sycl::device::get_devices();
-    for(auto d : devices) std::cout << d.get_info<sycl::info::device::name>() << "\n";
-
-    sycl::host_selector cpu;
-    sycl::queue q(cpu);
-
-    std::cout << "Device: "
-	      << q.get_device().get_info<sycl::info::device::name>()
-              << "\n";
-    
 
     int N = 5;
     int *f;
@@ -29,7 +20,7 @@ int main()
     *f = 1;
     factorial<<<1,1>>>(N,f);
     cudaDeviceSynchronize();
-    std::cout << *f << "\n";
+    cout << *f << "\n";
 
     return EXIT_SUCCESS;
 }
